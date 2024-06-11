9b5807140be43d263a4c4aefa267b2feb27675fb
sean <sean.g.crim@gmail.com>
2024-06-11 17:06:56 -0400

    began going through course

---
+++ styled/src/App.js
@@ -0,0 +1,13 @@
+import ComplexTitle from "./components/ComplexTitle";
+import Random from "./components/Random";
+
+function App() {
+  return (
+    <div style={{ padding:'2rem'}}>
+      <ComplexTitle title="more complex title" />
+      <Random />
+    </div>
+  );
+}
+
+export default App;
